de16ddf4216bd22bd4bc82982b912317996746a7
lenovo <pelmaksim1234@gmail.com>
2026-09-20 12:24:40 +0300

    створення папки веб

--- labs3/в14/4.js
+++
@@ -1,6 +0,0 @@
-function oddLengthStrings(arr) {
-    return arr.filter(str => str.length % 2 !== 0);
-}
-
-let arr = ["hi", "car", "майонез", "пес"];
-console.log(oddLengthStrings(arr));
--- labs3/в14/6.js
+++
@@ -1,6 +0,0 @@
-function checkNumbers(a, b) {
-    return (a + b === 10 || Math.abs(a - b) === 10);
-}
-
-console.log(checkNumbers(7,3));
-console.log(checkNumbers(20,10));
